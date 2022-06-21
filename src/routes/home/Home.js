aecd1968c2152a2e5478da98c35214589549de7a
Angelo Spampinato <angelo.spampinato@blockfi.com>
2022-06-21 13:51:22 -0700

    Fixing album list, making mobile version

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const Home = () => {
     const [albumData, setAlbumData] = useState([])
@@ -8,31 +8,64 @@ const Home = () => {
             .then(resp => resp.json())
             .then(albumData => {
                 setAlbumData(albumData)
+                console.log(albumData)
             })
     }, [])
 
-    const renderCards = () => (
-        albumData?.reverse().map(album => {
-            return (
-                <div id={`album-${album.id}`} className="album-container" key={album.id}>
-                    <a href={album.url} target="_blank" rel="noopenner noreferrer">
-                        <img src={`/assets/albums/${album.id}.jpg`}></img>
-                        <div className="overlay">
-                            <div className="album-text">
-                                <p>{album.title}</p>
-                                <p>{album.description}</p>
+    const handleAlbumClickMobile = (albumId) => {
+        const tempAlbumData = [...albumData]
+        const tempAlbum = {...tempAlbumData[albumId - 1]}
+
+        tempAlbumData.map(album => album.opacity = 0)
+
+        tempAlbum.opacity = 1
+        tempAlbumData[albumId - 1] = tempAlbum
+
+        console.log(tempAlbum, albumId)
+
+        setAlbumData(tempAlbumData)
+    }
+
+    const renderCards = useMemo(() => (
+        albumData?.map(album => {
+            if (/Mobi|Android/i.test(navigator.userAgent)) {
+                return (
+                    <div id={`album-${album.id}`} className="album-container" key={album.id}>
+                            <img src={`/assets/albums/${album.id}.jpg`}></img>
+                            <div onClick={() => handleAlbumClickMobile(album.id)} className="overlay" style={{ opacity: album.opacity }}>
+                                <div className="album-text">
+                                    <p>{album.title}</p>
+                                    <p>{album.description}</p>
+                                    <p><a href={album.url} target="_blank" rel="noopenner noreferrer">Listen</a></p>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
-            )
+                    </div>
+                )
+            } else {
+                return (
+                    <div id={`album-${album.id}`} className="album-container" key={album.id}>
+                        <a href={album.url} target="_blank" rel="noopenner noreferrer">
+                            <img src={`/assets/albums/${album.id}.jpg`}></img>
+                            <div className="overlay">
+                                <div className="album-text">
+                                    <p>{album.title}</p>
+                                    <p>{album.description}</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                )
+            }
         })
-    )
+    ), [albumData])
 
 	return (
 		<div className="home-container">
-			<h1>Low Noon</h1>
-            {renderCards()}
+            <div className="header-text">
+                <h1>Rafferty Swink</h1>
+                <h3>Musician, Producer, Mixer</h3>
+            </div>
+            {renderCards}
 		</div>
 	);
 }
